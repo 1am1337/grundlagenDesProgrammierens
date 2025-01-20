a9527c0cca71b704904b991455c958708c7af933
dkyuh <daniel.k.hopfner@gmail.com>
2025-01-20 17:06:11 +0100

    Resolved merge conflicts

--- print_greetings.py
+++ print_greetings.py
@@ -1,6 +1,6 @@
 
 # hier euren Namen (oder Nickname o.Ä.) hinzufügen
-names = ['Kurs', 'Daniel', 'Oskar', 'Fabian', 'Adrian', 'Michael', 'Moyie', 'sara']
+names = ['Kurs', 'Daniel', 'Oskar', 'Fabian', 'Adrian', 'Michael', 'Moyie', 'sara', 'Veronika']
 
 for name in names:
     print(f'Hallo, {name}!')
