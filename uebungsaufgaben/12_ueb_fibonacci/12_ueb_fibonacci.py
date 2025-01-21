0a4b24b958aa18f6c3646cc93e13d364b290b0a4
dkyuh <daniel.k.hopfner@gmail.com>
2025-01-21 12:37:41 +0100

    ueb_12

---
+++ uebungsaufgaben/12_ueb_fibonacci/12_ueb_fibonacci.py
@@ -0,0 +1,10 @@
+# Übungsaufgabe bis nächste Woche
+# 
+# In der sogenannten **Fibonacci-Zahlenreihe** sind die ersten beiden Zahlen
+# definiert als 0 und 1. die nächste Zahl wird jeweils berechnet, indem man die
+# beiden vorherigen Zahlen aufaddiert
+# (Beginn der Reihe: 0, 1, 1, 2, 3, 5, 8, 13, ...)
+# 
+# Schreibt zwei Funktionen, die die n-te Fibonaccizahl zurückgeben. Eine soll
+# iterativ, die andere rekursiv implementiert sein.
+
