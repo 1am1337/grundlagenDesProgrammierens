548c265ebae251bd015e224d30db00d193d77c30
1am1337 <oskar.nitzschker@icloud.com>
2025-01-23 12:27:26 +0100

    fixed being able to enter negative values

--- uebungsaufgaben/12_ueb_fibonacci/iterativOskar.py
+++ uebungsaufgaben/12_ueb_fibonacci/iterativOskar.py
@@ -1,14 +1,14 @@
 num1 = 0
 num2 = 1
-fibNum = 0
+fibNum = 1
 while True:
-	nthDegree = input("which fibonacchi number do you want to know?:")
+	nthDegree = input("which fibonacchi number do you want to know?: " )
 	if nthDegree.isdigit():
 		for i in range(int(nthDegree)-1):
 			fibNum = num1 + num2
 			num1 = num2 
 			num2 = fibNum
-		print(fibNum)
+		print(num1)
 		break
 	else:
 		print("not a number")
--- uebungsaufgaben/12_ueb_fibonacci/rekursivOskar.py
+++ uebungsaufgaben/12_ueb_fibonacci/rekursivOskar.py
@@ -1,9 +1,11 @@
 def _fibonacchi(nthDegree):
-	if nthDegree == 0:
+	if nthDegree == 1:
 		return 0
-	elif nthDegree == 1:
+	elif nthDegree == 2  or nthDegree == 3:
 		return 1
+	elif nthDegree <= 0:
+		return("error: value not possible")
 	else:
 	 	fibNum = _fibonacchi(nthDegree-1) + _fibonacchi(nthDegree-2) 
 	 	return fibNum
-print(_fibonacchi(int(input("which fibonacchi number do you want to know?:"))))
+print(_fibonacchi(int(input("which fibonacchi number do you want to know?: "))))
