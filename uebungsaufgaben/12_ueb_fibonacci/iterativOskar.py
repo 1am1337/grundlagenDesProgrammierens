9463280e5d9f403de5b0dd5ebea6398daff43d20
1am1337 <oskar.nitzschker@icloud.com>
2025-01-28 11:31:42 +0100

    1000000th fibonacchi number :)

--- uebungsaufgaben/12_ueb_fibonacci/iterativOskar.py
+++ uebungsaufgaben/12_ueb_fibonacci/iterativOskar.py
@@ -1,14 +1,11 @@
+import sys
+sys.set_int_max_str_digits(1000000)
 num1 = 0
 num2 = 1
 fibNum = 1
-while True:
-	nthDegree = input("which fibonacchi number do you want to know?: " )
-	if nthDegree.isdigit():
-		for i in range(int(nthDegree)-1):
-			fibNum = num1 + num2
-			num1 = num2 
-			num2 = fibNum
-		print(num1)
-		break
-	else:
-		print("not a number")
+nthDegree = 1000000
+for i in range(nthDegree):
+	fibNum = num1 + num2
+	num1 = num2 
+	num2 = fibNum
+print(num1)
