8bbfc6669ff699b21919d5ba88f5fac1a7783a76
1am1337 <oskar.nitzschker@icloud.com>
2025-01-30 11:37:03 +0100

    now only three lines of code :3

--- uebungsaufgaben/13_ueb_sets_scales/ha280125.py
+++ uebungsaufgaben/13_ueb_sets_scales/ha280125.py
@@ -1,5 +1,3 @@
-dDur = {"D","E", "FIS","G","A", "H","C"}
+dDur = {"D","E", "FIS","G","A", "H","CIS"}
 bDur = {"B", "C", "D", "ES", "F", "G", "A"}
-print("Diese Noten sind in beiden Tonleitern: ", dDur.intersection(bDur))
-print("Diese Noten gibt es nur in D-Dur: ", dDur.difference(bDur))
-print("Diese Noten gibt es nur in B-Dur: ", bDur.difference(dDur))
+print("Diese Noten sind in beiden Tonleitern: ", dDur.intersection(bDur), "\nDiese Noten gibt es nur in D-Dur: ", dDur.difference(bDur), "\nDiese Noten gibt es nur in B-Dur: ", bDur.difference(dDur))
