167bb146a8073c0d6a4e897a4467a648dc548a9b
1am1337 <oskar.nitzschker@icloud.com>
2025-01-30 12:12:58 +0100

    merged two files

--- uebungsaufgaben/13_ueb_sets_scales/13_ueb_sets_scales.py
+++ uebungsaufgaben/13_ueb_sets_scales/13_ueb_sets_scales.py
@@ -6,3 +6,7 @@
 # 	- Welche Töne beide Töne gemeinsam haben.
 # 	- Welche Töne es nur in D-Dur gibt.
 # 	- Welche Töne es nur in B-Dur gibt.
+dDur = {"D","E", "FIS","G","A", "H","CIS"}
+bDur = {"B", "C", "D", "ES", "F", "G", "A"}
+print("Diese Noten sind in beiden Tonleitern: ", dDur.intersection(bDur), "\nDiese Noten gibt es nur in D-Dur: ", dDur.difference(bDur), "\nDiese Noten gibt es nur in B-Dur: ", bDur.difference(dDur))
+
--- uebungsaufgaben/13_ueb_sets_scales/ha280125.py
+++
@@ -1,3 +0,0 @@
-dDur = {"D","E", "FIS","G","A", "H","CIS"}
-bDur = {"B", "C", "D", "ES", "F", "G", "A"}
-print("Diese Noten sind in beiden Tonleitern: ", dDur.intersection(bDur), "\nDiese Noten gibt es nur in D-Dur: ", dDur.difference(bDur), "\nDiese Noten gibt es nur in B-Dur: ", bDur.difference(dDur))
