221a4b66e6a04bf9e6b561e79337edc9674cf5cd
1am1337 <oskar.nitzschker@icloud.com>
2025-04-08 11:37:07 +0200

    two repo push locals test

--- print_greetings.py
+++ print_greetings.py
@@ -1,4 +1,3 @@
-
 # hier euren Namen (oder Nickname o.Ä.) hinzufügen
 names = ['Kurs', 'Daniel', 'Oskar', 'Fabian', 'Adrian', 'Michael', 'Moyie', 'sara', 'Veronika', 'Charlotta', 'Lasse', 'Camilo']
 
