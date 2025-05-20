38315268b6724f2cca4156724d8626ec3e799924
Daniel Höpfner <daniel.k.hopfner@gmail.com>
2025-05-20 16:52:30 +0200

    planning functionality

--- midi_toolbox/midi_toolbox.py
+++ midi_toolbox/midi_toolbox.py
@@ -1,3 +1,22 @@
 
-"""Midi-Toolbox"""
+"""Midi-Toolbox
+
+- [ ] MidiEvent
+    - [ ] MidiNote
+    - [ ] MidiRest
+- [ ] Sequence
+    - [ ] Oktave
+    - [ ] Notennamen
+    - [ ] transponieren
+    - [ ] Umkehrung
+    - [ ] Krebs
+    - [ ] Krebs-Umkehrung
+    - [ ] Intervalle zählen
+    - [ ] Noten zählen
+    - [ ] ...
+- [ ] Abspielen
+    - [ ] mtof <-> 
+- [ ] MidiDateien
+    """
+
 
