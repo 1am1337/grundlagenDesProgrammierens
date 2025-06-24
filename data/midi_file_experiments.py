cd2c95120fd105f1a319a78e25689360c4562034
Daniel Höpfner <daniel.k.hopfner@gmail.com>
2025-06-24 16:22:21 +0200

    midi-file reading

--- data/midi_file_experiments.py
+++ data/midi_file_experiments.py
@@ -1,30 +1,58 @@
 import mido
-
-# with open("/Users/danielhopfner/Desktop/grundlagen-des-programmierens-2024-2025/data/bwv772.mid", "rb") as file:
-# # with open("bwv772.mid", "rb") as file:
-#     content = file.read()
-#     print(content[14:14 + 22])
+from pathlib import Path
 
 
-midifile = mido.MidiFile('/Users/danielhopfner/Desktop/grundlagen-des-programmierens-2024-2025/data/bwv772.mid')
+midifile_path = Path(__file__).parent / 'bwv772.mid' # __file__: Default-Variable, die zur aktuellen Datei zeigt
+midifile = mido.MidiFile(midifile_path)
 
-# for i, msg in enumerate(midifile):
+for msg in midifile:
+    if msg.type == 'set_tempo':
+        tempo = msg.tempo
 
-#     print(msg)
 
-#     if i == 40:
-#         break
+# class MidiNote:
 
-# print()
-for track in midifile.tracks:
+#     def __init__(self, note, velocity, time_on, time_off):
+#         self.note = note
+#         self.velocity = velocity
+#         self.time_on = time_on
+#         self.time_off = time_off
 
-    print(f'Track Name: "{track.name}", Number of messages: {len(track)}')
+#     def __repr__(self):
+#         return f'<MidiNote: note: {self.note}, velocity: {self.velocity}, on: {self.time_on}, off: {self.time_off}>'
+
+
+onset_notes = {
+    # expected data structure:
+    # 60: [MidiNote(note=60, velocity=64, time_on=0, time_off=0)],
+}
+sequence = []
+abs_time = 0
 
 # track 1
 track1 = midifile.tracks[1]
 for i, msg in enumerate(track1):
 
-    if i < 40:
+        # msg.time sind immer die Zeit-Unterschiede zwischen den aufeinanderfolgenden Events (egal ob note-on oder -off)
+        abs_time += mido.tick2second(msg.time, midifile.ticks_per_beat, tempo)
 
         if msg.type == 'note_on':
-            print(msg.note)
+            onset_notes[msg.note] = [MidiNote(note=msg.note, velocity=msg.velocity, time_on=abs_time, time_off=0)]
+        if msg.type == 'note_off':
+            midinote = onset_notes.get(msg.note).pop(0)
+            midinote.time_off = abs_time
+            sequence.append(midinote)
+
+
+[print(note) for note in sequence]
+
+
+# Übungsaufgabe bis nächste Woche: 
+# MidiNote und MidiSequence aktualisieren, sodass sie mit dem
+# Auslesen der Midi-Files kompatibel sind (draft siehe die Klasse in dieser Datei)
+
+# --> nicht mehr die `MidiNote`-Klasse verwenden, die hier in der
+# Datei implementiert wurde, sondern:
+
+# from ..midi_toolbox.note import MidiNote
+# from ..midi_toolbox.sequence import MidiSequence
