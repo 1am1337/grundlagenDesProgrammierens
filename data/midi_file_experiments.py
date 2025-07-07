18095c21bf4bfebc37181aab24957684f51bbb6f
1am1337 <oskar.nitzschker@icloud.com>
2025-07-07 12:54:57 +0200

    ich weiß nicht mehr von wann die file ist :(

--- data/midi_file_experiments.py
+++ data/midi_file_experiments.py
@@ -1,30 +1,34 @@
 import mido
+import os
+from pathlib import Path
 
-# with open("/Users/danielhopfner/Desktop/grundlagen-des-programmierens-2024-2025/data/bwv772.mid", "rb") as file:
-# # with open("bwv772.mid", "rb") as file:
-#     content = file.read()
-#     print(content[14:14 + 22])
+midifile_path = Path(__file__). parent / "bwv772.mid"
+midifile = mido.MidiFile(midifile_path)
 
+class MidiNote:
+    def __init__(self, note, velocity, time_on, time_off):
+        self.note = note
+        self.velocity = velocity
+        self.time_on = time_on
+        self.time_off = time_off
+    def __repr__(self):
+        return f"<MidiNote: note: {self.note}, velocity: {self.velocity}, on: {self.time_on}, off: {self.time_off}>"
 
-midifile = mido.MidiFile('/Users/danielhopfner/Desktop/grundlagen-des-programmierens-2024-2025/data/bwv772.mid')
-
-# for i, msg in enumerate(midifile):
-
-#     print(msg)
-
-#     if i == 40:
-#         break
-
-# print()
-for track in midifile.tracks:
-
-    print(f'Track Name: "{track.name}", Number of messages: {len(track)}')
-
-# track 1
+onset_notes = {}
+sequence = []
 track1 = midifile.tracks[1]
+abs_time = 0
+for msg in midifile:
+    if msg.type == "set_tempo":
+        tempo = msg.tempo
+
+
 for i, msg in enumerate(track1):
-
-    if i < 40:
-
-        if msg.type == 'note_on':
-            print(msg.note)
+    abs_time += mido.tick2second(msg.time, midifile.ticks_per_beat, tempo)
+    if msg.type == "note_on":
+        onset_notes[msg.note] = [MidiNote(note=msg.note, velocity=msg.velocity, time_on=abs_time, time_off=0)]
+    if msg.type == "note_off":
+        midinote = onset_notes.get(msg.note).pop(0)
+        midinote.time_off = abs_time
+        sequence.append(midinote)
+[print(note) for note in sequence]
